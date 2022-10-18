90248b756b84bcd1a64b9f417799e827a7e5dcdb
Sabrinashh <110469877+Sabrinashh@users.noreply.github.com>
2022-10-18 23:51:45 +0400

    file

--- hw-19.10 p-2/assets/main.js
+++ hw-19.10 p-2/assets/main.js
@@ -1,26 +1,49 @@
 let seconds = document.querySelector("#seconds");
-       seconds.addEventListener('click',e=>{
-            e.preventDefault();
-        })
+let minute=document.querySelector("#min");
 let start = document.getElementById('start');
  start.addEventListener('onclick', e=>{
-int = setInterval(()=>{
- second++;
- seconds.innerText = second;
-},1000)
+    clearInterval(int);
 })
-let second = 0;
-let int = setInterval(()=>{
-second++;
-seconds.innerText = second;
-},1000)
+let second = 00;
+let min = 00;
+let int = setInterval(startTimer,1000);
+
 let btnstop = document.getElementById('stop');
 btnstop.onclick = function() {
     clearInterval(int);
 }
+
 let btnreset = document.getElementById('reset');
 btnreset.onclick = function() {
     clearInterval(int);
      second = "00";
+     min="00"
      seconds.innerHTML = second;
+    minute.innerHTML = min;
  }
+  
+ function startTimer () {
+    second++; 
+    
+    if(second <= 9){
+      seconds.innerHTML = "0" + second;
+    }
+
+    if (second > 9){
+      seconds.innerHTML = second;
+    } 
+
+    if (second > 59) {
+      min++;
+      minute.innerHTML = "0" + min;
+      second = 0;
+      seconds.innerHTML = "0" + 0;
+    }
+
+    if (min > 9){
+        minute.innerHTML = min;
+    } 
+  }
+  
+  
+
